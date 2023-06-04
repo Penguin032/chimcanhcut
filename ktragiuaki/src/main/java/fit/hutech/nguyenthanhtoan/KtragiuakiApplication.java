88a18c7d364043dd75eeb9e4f118ac8aa49ae7f2
Penguin032 <135446517+Penguin032@users.noreply.github.com>
2023-06-04 20:42:43 +0700

    first commit

---
+++ ktragiuaki/src/main/java/fit/hutech/nguyenthanhtoan/KtragiuakiApplication.java
@@ -0,0 +1,13 @@
+package fit.hutech.nguyenthanhtoan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KtragiuakiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KtragiuakiApplication.class, args);
+	}
+
+}
